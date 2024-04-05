b7b77c98b6cbb4d78bbf92a144c5b784c2ce9c39
TYT_AKHILP <akhilise.rymec@gmail.com>
2024-04-05 14:58:58 +0530

    initial commit

---
+++ utils/database.js
@@ -0,0 +1,52 @@
+// import mongoose from "mongoose";
+
+// let isConnected = false;
+
+// export const connectToDB = async() =>{
+//     mongoose.set('strictQuery', true);
+
+//     if(isConnected){
+//         console.log('Mongo connected')
+//         return;
+//     }
+
+//     try{
+//         await mongoose.connect(process.env.MONGODB_URI,{
+//             dbName : 'share_prompt',
+//             useNewUrlParser : true,
+//             useUnifiedTopology : true
+//         })
+//         isConnected = true;
+//         console.log('Mongo connected')
+//     }
+//     catch(error){
+
+//     }
+// }
+
+import mongoose from 'mongoose';
+
+let isConnected = false; // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
+}
